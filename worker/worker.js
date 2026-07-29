5636cd56ae9fe3e4fca7837af5df97010984e854
polskiftw <polskiftw@gmail.com>
2026-07-29 02:25:37 -0400

    Add Content Security Policy

--- worker/worker.js
+++ worker/worker.js
@@ -10,6 +10,20 @@ const PERMISSIONS_POLICY = [
   "payment=()",
   "usb=()",
 ].join(", ");
+const CONTENT_SECURITY_POLICY = [
+  "default-src 'none'",
+  "script-src 'self' 'unsafe-inline'",
+  "style-src 'self' 'unsafe-inline'",
+  "img-src 'self' data: blob:",
+  "media-src 'self' blob:",
+  "connect-src 'self'",
+  "font-src 'self'",
+  "object-src 'none'",
+  "base-uri 'none'",
+  "form-action 'none'",
+  "frame-ancestors 'none'",
+  "manifest-src 'none'",
+].join("; ");
 
 export default {
   async fetch(request, env, ctx) {
@@ -18,6 +32,7 @@ export default {
 
     headers.set("x-robots-tag", ROBOTS_POLICY);
     headers.set("permissions-policy", PERMISSIONS_POLICY);
+    headers.set("content-security-policy", CONTENT_SECURITY_POLICY);
     headers.set("x-content-type-options", "nosniff");
     headers.set("referrer-policy", "no-referrer");
 

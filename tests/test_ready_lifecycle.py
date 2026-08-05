9d5d2581308c1196d7e5b35e17e3398a58cf330b
polskiftw <polskiftw@gmail.com>
2026-08-05 04:29:54 -0400

    Test session and permanent exclusion modes

--- tests/test_ready_lifecycle.py
+++ tests/test_ready_lifecycle.py
@@ -27,19 +27,28 @@ class ReadyLifecycleTests(unittest.TestCase):
         self.database.close()
         self.temp.cleanup()
 
-    def test_exclusion_survives_rebuild_and_orientation_change(self) -> None:
+    def test_session_exclusion_is_forgotten_on_rebuild(self) -> None:
         self.database.replace_pairs([("a.jpg", "b.jpg", 90.0, "first")])
-        pair = self.database.scan_pairs()[0]
-        self.assertTrue(self.database.exclude_pair_keys(pair.left_key, pair.right_key))
+        self.assertTrue(self.database.exclude_pair_keys("a.jpg", "b.jpg"))
+        self.assertEqual(self.database.queued_deletions(), [])
+
+        self.database.replace_pairs([("a.jpg", "b.jpg", 91.0, "next run")])
+        rebuilt = self.database.scan_pairs()[0]
+        self.assertNotEqual(rebuilt.status, "excluded")
+        self.assertEqual(len(self.database.queued_deletions()), 1)
+
+    def test_permanent_exclusion_survives_rebuild_and_orientation_change(self) -> None:
+        self.database.replace_pairs([("a.jpg", "b.jpg", 90.0, "first")])
+        self.assertTrue(self.database.exclude_pair_permanently("a.jpg", "b.jpg"))
 
         self.database.replace_pairs([("b.jpg", "a.jpg", 91.0, "reoriented")])
         rebuilt = self.database.scan_pairs()[0]
         self.assertEqual(rebuilt.status, "excluded")
         self.assertEqual(self.database.queued_deletions(), [])
 
-    def test_exclusion_survives_database_reopen(self) -> None:
+    def test_permanent_exclusion_survives_database_reopen(self) -> None:
         self.database.replace_pairs([("a.jpg", "b.jpg", 90.0, "first")])
-        self.database.exclude_pair_keys("a.jpg", "b.jpg")
+        self.database.exclude_pair_permanently("a.jpg", "b.jpg")
         self.database.close()
 
         self.database = Database(self.path)
@@ -47,9 +56,9 @@ class ReadyLifecycleTests(unittest.TestCase):
         self.database.replace_pairs([("a.jpg", "b.jpg", 92.0, "after restart")])
         self.assertEqual(self.database.scan_pairs()[0].status, "excluded")
 
-    def test_deleted_asset_purges_obsolete_decision(self) -> None:
+    def test_deleted_asset_purges_obsolete_permanent_decision(self) -> None:
         self.database.replace_pairs([("a.jpg", "b.jpg", 90.0, "first")])
-        self.database.exclude_pair_keys("a.jpg", "b.jpg")
+        self.database.exclude_pair_permanently("a.jpg", "b.jpg")
         self.database.record_deletions([("b.jpg", None, 200, "deleted")])
 
         row = self.database.connection.execute(
